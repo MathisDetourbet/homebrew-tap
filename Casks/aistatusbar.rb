bdcbcbb847c99b7c83bf30c6d90e0fc23c6b01d0
Mathis Detourbet <mathis.detourbet@gmail.com>
2026-03-25 22:41:21 +0100

    aistatusbar 0.2.0 - update SHA-256

--- Casks/aistatusbar.rb
+++ Casks/aistatusbar.rb
@@ -1,6 +1,6 @@
 cask "aistatusbar" do
   version "0.2.0"
-  sha256 "PLACEHOLDER"
+  sha256 "f9949546beac6f8fa74b04bb398576d315be60b3954844ba8228f1c033a169da"
 
   url "https://github.com/MathisDetourbet/AIStatus/releases/download/v#{version}/AIStatusBar-#{version}-macos-universal.zip"
   name "AIStatusBar"
