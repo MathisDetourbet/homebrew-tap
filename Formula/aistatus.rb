e31b7b857fc861a6cdf4598329e29ccb54ef7443
Mathis Detourbet <MathisDetourbet@users.noreply.github.com>
2026-09-04 12:24:49 +0200

    aistatus 0.5.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/aistatus.rb
+++ Formula/aistatus.rb
@@ -1,8 +1,8 @@
 class Aistatus < Formula
   desc "Check AI service status from the command line"
   homepage "https://github.com/MathisDetourbet/AIStatus"
-  url "https://github.com/MathisDetourbet/AIStatus/releases/download/v0.5.0/aistatus-0.5.0-macos-universal.tar.gz"
-  sha256 "c0babe9a8b055475e02584c79221e47aa26bd737c1cd67eff9f1e42b0f340a92"
+  url "https://github.com/MathisDetourbet/AIStatus/releases/download/v0.5.1/aistatus-0.5.1-macos-universal.tar.gz"
+  sha256 "4ec71b3d080691878f3c8955c0b399b2b7b8728e612ca7a3b19ccfa8e50df7d3"
   license "MIT"
 
   depends_on :macos
